6f5454dbb925a38c9bea50991223281979f97757
Cao Hoàng Huy <hoanghuychh@gmail.com>
2022-05-25 10:17:50 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import htmlToPdfmake from 'html-to-pdfmake';
+import jsPDF from 'jspdf';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'htmltopdf';
+  title = 'htmltopdf12';
+
+  @ViewChild('pdfTable') pdfTable: ElementRef;
+  //PDF genratwe button click function
+  public downloadAsPDF() {
+    const doc = new jsPDF();
+    //get table html
+    const pdfTable = this.pdfTable.nativeElement;
+
+    //html to pdf format
+    var html = htmlToPdfmake(pdfTable.innerHTML);
+   
+    const documentDefinition = { content: html };
+
+    pdfMake.createPdf(documentDefinition).open();
+
+    
+
+  
+  }
+
 }
